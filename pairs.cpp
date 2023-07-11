97f5422535123a28570ac615efdda552f96df5bd
Abh2004 <asm2004@kgpian.iitkgp.ac.in>
2023-07-11 16:19:01 +0530

    vectors

--- pairs.cpp
+++ pairs.cpp
@@ -4,6 +4,22 @@ int main(){
     pair<int,string> p ;
     // p = make_pair(2 , "abc");
     p = {2,"abhishek"};
+    pair<int,string> &p1 = p;
+
+    p1.first = 420 ;
+
+
     cout<<p.first<<" "<<p.second<<endl;
+
+    pair<int , int> p_array[3];
+
+    for(int i = 0 ; i< 3 ; i++)
+    {
+        cin>>p_array[i].first >> p_array[i].second;
+    }
+
+    for(int i = 0 ; i<3 ; i++){
+        cout<<p_array[i].first<<" "<<p_array[i].second<<endl;
+    }
 
 }
